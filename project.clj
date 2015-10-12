14a4a6ea1cb1c34374f2a0881039ddf8d1b1a805
Yusup Ashrap <awklsgrep@gmail.com>
2015-10-12 14:29:14 +0600

    init lambda

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [com.amazonaws/aws-lambda-java-core "1.0.0"]
+                 ]
   :main ^:skip-aot almiqi.core
   :target-path "target/%s"
+  :aot :all
   :profiles {:uberjar {:aot :all}})
